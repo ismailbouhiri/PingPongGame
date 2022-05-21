ae6badb315683ef2356a1e139ef462d2dfb5b0f3
Ismail Bouhiri <ibouhiri@e2r9p8.1337.ma>
2022-05-21 18:44:31 +0100

    resize images is done

--- game.js
+++ game.js
@@ -1,25 +1,41 @@
 var config = {
     type: Phaser.AUTO,
     width: 1920,
-    height: 1080,
+    hieght: 1120,
     scene: {
         preload: preload,
         create: create,
-        update: update
+        update: update,
+        resize: resize,
     }
-};
+}; 
 
 var game = new Phaser.Game(config);
+
+window.addEventListener('resize', () => {
+    game.resize(window.innerWidth, window.innerHeight);
+}, false);
+
+function resize(width, height) 
+{
+  this.cameras.resize(width, height);
+  this.bg.setDisplaySize(width, height);
+  // this.logo.setPosition(width / 2, height / 2);
+}
 
 function preload ()
 {
     this.load.image('table', 'assets/images/table.png');
 }
 
-function create ()
+function create()
 {
-    this.add.image(0, 0, 'table').setOrigin(0, 0);
+    // resize the images to fit the window 
+    this.events.on('resize', resize, this);
+    this.bg = this.add.sprite(game.config.width / 2, game.config.height / 2, 'table');
+    this.bg.setDisplaySize(game.config.width, game.config.height);
 }
+
 
 function update ()
 {
